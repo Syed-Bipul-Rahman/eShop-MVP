575a55d03052b9e007dbbe52b2b6f41a2236a534
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-10-06 22:49:23 +0600

    view binding added on details page, unecessery code removed

--- app/src/main/java/com/classjob/eshop_mvp/MainActivity.java
+++ app/src/main/java/com/classjob/eshop_mvp/MainActivity.java
@@ -1,14 +1,30 @@
 package com.classjob.eshop_mvp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.classjob.eshop_mvp.Adapters.HomeAdapter;
+import com.classjob.eshop_mvp.Model.Product;
+import com.classjob.eshop_mvp.Model.ProductsResponse;
+import com.classjob.eshop_mvp.Networks.ApiClient;
 import com.classjob.eshop_mvp.Networks.ApiInterface;
 import com.classjob.eshop_mvp.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.Call;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -29,6 +45,250 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        getSupportActionBar().hide();
+        initializing();
+
+
+        //bydefaoult all product will show
+        getdata();
+
+
+       // show category after click btn and them work respective users click
+        binding.categoryshowbutn.setOnClickListener(v -> {
+            binding.categoryitems.setVisibility(View.VISIBLE);
+            binding.recyclerview.setVisibility(View.GONE);
+
+            //   Toast.makeText(this, "Category", Toast.LENGTH_SHORT).show();
+        });
+
+        //handle after all product button
+        binding.allproductshowbutn.setOnClickListener(v -> {
+            getdata();
+            binding.categoryitems.setVisibility(View.GONE);
+            recyclerView.setVisibility(View.VISIBLE);
+
+        });
+
+
+        //show categorywise product after click on perticular category items
+        binding.laptop.setOnClickListener(v -> {
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/laptops");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+
+            //Toast.makeText(this, "Laptop clicked", Toast.LENGTH_SHORT).show();
+        });
+
+        binding.smartphonesview.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/smartphones");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.fragrances.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/fragrances");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.skincare.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/skincare");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.groceries.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/groceries");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.homedecoration.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/home-decoration");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.furniture.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/furniture");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.tops.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/tops");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.womendresse.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/womens-dresses");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.womenshoes.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/womens-shoes");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.mansshirt.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/mens-shirts");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.mansshoes.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/mens-shoes");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.manwatches.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/mens-watches");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.womenwatches.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/womens-watches");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.womenbags.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/womens-bags");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.womenbags.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/womens-bags");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.juwellery.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/womens-jewellery");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.glasses.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/sunglasses");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.automotive.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/automotive");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.motorcycle.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/motorcycle");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+        binding.lighting.setOnClickListener(v -> {
+
+            binding.categoryitems.setVisibility(View.GONE);
+            showCatitems("products/category/lighting");
+            binding.recyclerview.setVisibility(View.VISIBLE);
+        });
+
 
     }
-}
+
+    private void showCatitems(String catUrl) {
+        apiInterface.getCategoryproducts(catUrl).enqueue(new Callback<ProductsResponse>() {
+            @Override
+            public void onResponse(Call<ProductsResponse> call, Response<ProductsResponse> response) {
+                if (response.isSuccessful()) {
+
+                    binding.progressbarContainer.setVisibility(View.GONE);
+
+
+                    recyclerView.setVisibility(View.VISIBLE);
+                    List<Product> products = response.body().getProducts();
+                    setDataAdapter((ArrayList<Product>) products);
+
+
+                    dataAdapter.notifyDataSetChanged();
+                } else {
+                    Toast.makeText(MainActivity.this, "Failed to fetch data", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ProductsResponse> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Network error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+
+    private void initializing() {
+
+        recyclerView = binding.recyclerview;
+        recyclerView.setHasFixedSize(true);
+
+
+        Retrofit retrofit = ApiClient.getApiClient();
+        apiInterface = retrofit.create(ApiInterface.class);
+    }
+
+    private void getdata() {
+
+        apiInterface.getAllProducts().enqueue(new Callback<ProductsResponse>() {
+            @Override
+            public void onResponse(Call<ProductsResponse> call, Response<ProductsResponse> response) {
+                if (response.isSuccessful()) {
+
+                    binding.progressbarContainer.setVisibility(View.GONE);
+
+
+                    recyclerView.setVisibility(View.VISIBLE);
+                    List<Product> products = response.body().getProducts();
+                    setDataAdapter((ArrayList<Product>) products);
+
+                    dataAdapter.notifyDataSetChanged();
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Failed to fetch data", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ProductsResponse> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Network error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+
+    private void setDataAdapter(ArrayList<Product> dataModels) {
+        dataAdapter = new HomeAdapter(this, dataModels);
+
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        gridLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        //adapter
+
+        recyclerView.setAdapter(dataAdapter);
+
+
+    }
+
+
+
+    }
--- app/src/main/java/com/classjob/eshop_mvp/Views/ProductDetails.java
+++ app/src/main/java/com/classjob/eshop_mvp/Views/ProductDetails.java
@@ -1,21 +1,188 @@
 package com.classjob.eshop_mvp.Views;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.classjob.eshop_mvp.Model.Product;
+import com.classjob.eshop_mvp.Networks.ApiInterface;
 import com.classjob.eshop_mvp.R;
+import com.classjob.eshop_mvp.databinding.ActivityProductDetailsBinding;
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ProductDetails extends AppCompatActivity {
+    ActivityProductDetailsBinding binding;
+    ApiInterface apiInterface;
+    String id, pronameint;
+
+    ImageSlider imageSlider;
+    double productPrice;
+    int defaultquantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_details);
+        binding = ActivityProductDetailsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+        setContentView(view);
 
 
+        getSupportActionBar().hide();
 
+        //init views
+
+
+        imageSlider = findViewById(R.id.img_slid);
+
+
+        Intent intent = getIntent();
+        id = intent.getStringExtra("id");
+        pronameint = intent.getStringExtra("proname");
+
+//split product name 2 words
+        String[] split = pronameint.split(" ");
+
+
+        if (split.length > 2) {
+            String firstWord = split[0];
+            String secondWord = split[1];
+            binding.productnametitlebar.setText(firstWord + " " + secondWord);
+        } else {
+            binding.productnametitlebar.setText(pronameint);
+        }
+
+
+        //retrofit
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://dummyjson.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        apiInterface = retrofit.create(ApiInterface.class);
+        getDetails();
+
+
+        binding.backArroworder.setOnClickListener(v -> {
+            finish();
+        });
+
+        binding.quantityTextview.setText(defaultquantity + "");
+        //increase decrease functionality
+        //for increase
+        binding.increaseQuantitybtn.setOnClickListener(v -> {
+            defaultquantity += 1;
+            binding.quantityTextview.setText(defaultquantity + "");
+
+            productPrice *= defaultquantity;
+            binding.totaltopayprice.setText("$" + productPrice);
+
+        });
+        //for decrease
+        binding.decreaseQuantitybtn.setOnClickListener(v -> {
+            if (defaultquantity > 1) {
+                productPrice /= defaultquantity;
+                defaultquantity -= 1;
+                binding.quantityTextview.setText(defaultquantity + "");
+                binding.totaltopayprice.setText("$" + productPrice);
+            }
+
+        });
+
+
+        //show dialogue
+        binding.orderNow.setOnClickListener(v -> {
+            showAlert("Order has been successfully placed. You have to pay " + productPrice + "$ After receive the product.");
+        });
+        binding.addToCart.setOnClickListener(v -> {
+            showAlert("Product Successfully added to your cart.");
+        });
 
 
     }
+
+    private void showAlert(String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ProductDetails.this);
+        builder.setTitle("Success");
+        builder.setIcon(R.drawable.ic_baseline_check_circle_24);
+        builder.setMessage(msg);
+        builder.setPositiveButton("Ok", (dialog, which) -> {
+            dialog.dismiss();
+        });
+        builder.setNegativeButton("Cancel", (dialog, which) -> {
+            dialog.dismiss();
+        });
+        builder.show();
+
+    }
+
+
+    private void getDetails() {
+        Call<Product> callAllProducts = apiInterface.getSingleProducts("products/" + id);
+        callAllProducts.enqueue(new Callback<Product>() {
+            @Override
+            public void onResponse(Call<Product> call, Response<Product> response) {
+
+                if (response.isSuccessful()) {
+                    //hide progress bar
+                    binding.linearLayout.setVisibility(View.VISIBLE);
+                    binding.showloadin.setVisibility(View.GONE);
+
+                    //get price for conversion
+                    productPrice = response.body().getPrice();
+                    String productname = response.body().getTitle();
+
+
+                    binding.namedetails.setText(productname);
+                    binding.branddetails.setText(response.body().getBrand());
+                    binding.categorydetails.setText(response.body().getCategory());
+                    binding.stockdetails.setText("Stock: " + response.body().getStock());
+                    binding.totaltopayprice.setText("$" + productPrice);
+                    binding.ratingdetails.setText(response.body().getRating() + " ");
+
+                    binding.pricedetails.setText("$" + productPrice);
+                    binding.descdetails.setText(response.body().getDescription());
+
+//                    //show image using picasso
+//                    String imageUrl = response.body().getThumbnail();
+//                    Picasso.get().load(imageUrl).into(productimage);
+
+                    // Load product images into ImageSlider
+                    List<String> productImages = response.body().getImages();
+                    List<SlideModel> imageList = new ArrayList<>();
+                    for (String imageUrl : productImages) {
+                        imageList.add(new SlideModel(imageUrl, ScaleTypes.CENTER_INSIDE));
+                    }
+                    imageSlider.setImageList(imageList);
+                    //  Log.d("images",imageList.toString());
+
+                } else {
+                    Toast.makeText(ProductDetails.this, "Failed to fetch data", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Call<Product> call, Throwable t) {
+                Toast.makeText(ProductDetails.this, "Failed to connect with server", Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+    }
+
 }
